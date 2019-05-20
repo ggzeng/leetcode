886158f31a960acd161a5b72abe5530e8f49b437
Zeng Ganghui <ganghui.zeng@tongdun.net>
2019-05-20 14:41:59 +0800

    1 两数之和
    clang for.for

---
+++ clang/1_2_twoSum.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hashSize;
+int *hashBuckets;
+
+int hashInit(int size) {
+    
+}
+
+int hashDestroy(int *hash) {
+    
+}
+
+int hashSave(int key, int val) {
+    
+}
+
+int hashGet(int key) {
+    
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i, j;
+    int num1, num2;
+    int *returnNums = (int *)malloc(sizeof(int)*2);
+    if (!returnNums) {
+        return NULL;
+    }
+    for (i = 0; i < numsSize; i++) {
+        num1 = nums[i];
+        for (j = i+1; j < numsSize; j++) {
+            num2 = nums[j];
+            if ((num1 + num2) == target) {
+                returnNums[0] = i;
+                returnNums[1] = j;
+                *returnSize = 2;
+                return returnNums;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
+
+int main(void) {
+    int rtnSize;
+    int nums[] = {2, 7, 11, 15};
+    int *rtnNums;
+    rtnNums = twoSum((int *)nums, 4, 9, &rtnSize);
+    if (rtnNums) {
+        printf("[%d, %d]\n", rtnNums[0], rtnNums[1]);
+        free(rtnNums);
+    } else {
+        printf("not found!\n");
+    }
+}
